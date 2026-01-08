299bc029c8b2e12b3eb15f03eeb5c9e23c25dd88
pavithra-hn <pavithrahn56@gmail.com>
2026-01-08 18:58:26 +0530

    java task

---
+++ java/Linkedlist/p7.java
@@ -0,0 +1,17 @@
+//Store and print student names
+
+import java.util.LinkedList;
+
+public class p7 {
+    public static void main(String[] args) {
+        LinkedList<String> students = new LinkedList<>();
+        students.add("Pavi");
+        students.add("Anu");
+        students.add("Ravi");
+
+        System.out.println(students);
+    }
+}
+
+
+//Output: [Pavi, Anu, Ravi]
---
+++ java/for_loop/p1.java
@@ -0,0 +1,14 @@
+public class p1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// output :
+// 1
+// 2
+// 3
+// 4
+// 5
